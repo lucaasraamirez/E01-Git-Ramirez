bf202c9f799e46f080878f00d82c34edbbd8c704
lucaasraamirez <80787727+lucaasraamirez@users.noreply.github.com>
2026-04-07 21:23:29 -0300

    Add files via upload

---
+++ Dev1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    int numero1, numero2;
+
+    printf("Debe ingresar dos numeros\n");
+
+    printf("ingrese el primer numero:");
+    scanf("%d", &numero1);
+
+    printf("Ingrese el segundo numero\n");
+    scanf("%d", &numero2);
+
+    printf("El primer numero es:%d\n", numero1);
+    printf("El segundo numero es:%d\n", numero2);
+
+    return 0;
+}
